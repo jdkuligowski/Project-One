fcc03ad1c543f364e47a703050bf6265a1456f53
James Kuligowski <jdkuligowski@gmail.com>
2022-04-08 11:40:58 +0100

    Project One Day 2 - characters moving across page

--- JavaScript/app.js
+++ JavaScript/app.js
@@ -11,20 +11,40 @@ function init() {
   const cellCount = width * height
   const cells = []
 
+  // Timing variables
+  let obstacleTimer
+
+
+
   function createGrid() {
     for (let i = 0; i < cellCount; i++) {
       const cell = document.createElement('div')
       cell.innerText = cell.id = i
       grid.appendChild(cell)
       cells.push(cell)
+
     }
-    
     addSimba(simbaStartPosition)
     addTimon(timonStartPosition)
     addPumbaa(pumbaaStartPosition)
-    addWater(waterPosition)
+    addWater(26); addWater(27); addWater(28); addWater(29); addWater(30)
+    addWater(31); addWater(32); addWater(33); addWater(34); addWater(35)
+    addWater(36); addWater(37); addWater(38); addWater(65); addWater(66)
+    addWater(67); addWater(68); addWater(69); addWater(70); addWater(71)
+    addWater(72); addWater(73); addWater(74); addWater(75); addWater(76)
+    addWater(77)
+    addHyena(hyenaStartPosition)
+    addScar(scarStartPosition)
+    addWildebeest(wildeStartPosition)
+    addZazu(26); addZazu(29); addZazu(32); addZazu(35); addZazu(38); 
   }
-  
+
+  // function waterFeatures () {
+  //   for (let i = width * 2; i < (width * 3) - 1; i++) {
+  //     cells[i].classList.add(waterClass)
+  //   } 
+  // }
+  // waterFeatures()
 
   // Character setup
   const simbaClass = 'simba'
@@ -41,7 +61,23 @@ function init() {
   let pumbaaCurrentPosition = pumbaaStartPosition
 
   const waterClass = 'water'
-  const waterPosition = width * 2
+  let waterPosition = []
+
+  const hyenaClass = 'hyena'
+  const hyenaStartPosition = 51
+  let hyenaCurrentPosition = hyenaStartPosition
+
+  const scarClass = 'scar'
+  const scarStartPosition = 22
+  let scarCurrentPosition = scarStartPosition
+
+  const wildeClass = 'wildebeest'
+  const wildeStartPosition = 89
+  let wildeCurrentPosition = wildeStartPosition
+
+  const zazuClass = 'zazu'
+
+
 
   // * Functions for adding players and to help them move
   // Add simba function
@@ -61,6 +97,56 @@ function init() {
     cells[position].classList.add(waterClass)
   }
 
+  // Hyena movement function
+  function addHyena() {
+    cells[hyenaCurrentPosition].classList.add(hyenaClass)
+    console.log(hyenaCurrentPosition)
+    obstacleTimer = setInterval(() => {
+      if (hyenaCurrentPosition <= width * 3) {
+        cells[hyenaCurrentPosition].classList.remove(hyenaClass)
+        hyenaCurrentPosition = hyenaStartPosition + 1
+      } else {
+        cells[hyenaCurrentPosition].classList.remove(hyenaClass)
+        hyenaCurrentPosition--
+        cells[hyenaCurrentPosition].classList.add(hyenaClass)
+      }
+    }, 900)
+  }
+
+  function addScar() {
+    cells[scarCurrentPosition].classList.add(scarClass)
+    console.log(scarCurrentPosition)
+    obstacleTimer = setInterval(() => {
+      if (scarCurrentPosition <= width) {
+        cells[scarCurrentPosition].classList.remove(scarClass)
+        scarCurrentPosition = 26
+      } else {
+        cells[scarCurrentPosition].classList.remove(scarClass)
+        scarCurrentPosition--
+        cells[scarCurrentPosition].classList.add(scarClass)
+      }
+    }, 1000)
+  }
+
+  function addWildebeest() {
+    cells[wildeCurrentPosition].classList.add(wildeClass)
+    console.log(wildeCurrentPosition)
+    obstacleTimer = setInterval(() => {
+      if (wildeCurrentPosition <= width * 6) {
+        cells[wildeCurrentPosition].classList.remove(wildeClass)
+        wildeCurrentPosition = 91
+      } else {
+        cells[wildeCurrentPosition].classList.remove(wildeClass)
+        wildeCurrentPosition--
+        cells[wildeCurrentPosition].classList.add(wildeClass)
+      }
+    }, 800)
+  }
+
+  function addZazu(position) {
+    cells[position].classList.add(zazuClass)
+  }
+
   // * Functions for removing players from previous square, so only one image appears
   // Add simba function
   function removeSimba(position) {
@@ -74,6 +160,15 @@ function init() {
   function removePumbaa(position) {
     cells[position].classList.remove(pumbaaClass)
   }
+
+  // Remove Hyena function
+  function removeHyena(position) {
+    cells[position].classList.remove(hyenaClass)
+  }
+
+
+
+
 
 
 
@@ -142,6 +237,27 @@ function init() {
   }
 
   document.addEventListener('keydown', handleKeyDown)
+
+
+
+
+
+
+  // Function to start the game and get the animals moving across the page
+  // function startGame() {
+  //   addHyena(hyenaStartPosition)
+  // hyenaCurrentPosition--
+  // obstacleTimer = setInterval(() => {
+  //   hyenaCurrentPosition --
+  //   removeHyena(hyenaCurrentPosition)
+  // },2000)
+  // }
+
+  // startGame()
+
+
+
+
 
 
 
